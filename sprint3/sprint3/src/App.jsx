2c3939a96346bf3ab96f13fdde852f44f11bc9ca
PedroSanagiotto <rm93182@fiap.com.br>
2022-09-05 20:10:42 -0300

    pull request

--- sprint3/sprint3/src/App.jsx
+++ sprint3/sprint3/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App (){
     return(
         <>
-            <h1>meu app</h1>
+            <h1>Meu App</h1>
             <h2>subtitulo</h2>
         </>
     )
